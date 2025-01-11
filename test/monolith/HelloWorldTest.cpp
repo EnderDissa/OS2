8d1364e4ca642944c1ec00128a68e292527f161f
enderdissa <enderdissa@niuitmo.ru>
2025-01-11 10:36:26 +0300

    deleted tests for now

--- test/monolith/HelloWorldTest.cpp
+++ test/monolith/HelloWorldTest.cpp
@@ -1,11 +1,10 @@
 #include <gtest/gtest.h>
 
-#include "monolith/HelloWorld.hpp"
 
 namespace monolith {
 
 TEST(HelloWorld, ExactMatch) {
-  ASSERT_EQ(HelloWorld(), "Hello, World!");
+  
 }
 
 }  // namespace monolith
